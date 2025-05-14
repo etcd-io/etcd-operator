9120a1aaf64ed5e9fb4e6bffd032a15416487be0
ArkaSaha30 <arkasaha30@gmail.com>
2025-05-14 12:33:39 +0530

    Add cert-manager interface function implementation
    This commit will implement the interface functions required for cert-manager
    certificate provider.
    
    Signed-off-by: ArkaSaha30 <arkasaha30@gmail.com>

---
+++ pkg/certificate/cert_manager/provider.go
@@ -0,0 +1,305 @@
+package cert_manager
+
+import (
+	"bytes"
+	"context"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/ed25519"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+
+	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	interfaces "go.etcd.io/etcd-operator/pkg/certificate/interfaces"
+)
+
+const (
+	IssuerNameKey = "issuerName"
+	IssuerKindKey = "issuerKind"
+)
+
+type CertManagerProvider struct {
+	client.Client
+}
+
+var _ interfaces.Provider = (*CertManagerProvider)(nil)
+
+func New(c client.Client) *CertManagerProvider {
+	return &CertManagerProvider{
+		c,
+	}
+}
+
+func (cm *CertManagerProvider) EnsureCertificateSecret(
+	ctx context.Context,
+	secretName string,
+	namespace string,
+	cfg *interfaces.Config) error {
+	valErr := cm.ValidateCertificateSecret(ctx, secretName, namespace, cfg)
+	if valErr != nil {
+		if k8serrors.IsNotFound(valErr) {
+			log.Printf("certificate secret: %s not present in namespace: %s , creating new Certificate",
+				secretName, namespace)
+			return cm.createCertificate(ctx, secretName, namespace, cfg)
+		} else {
+			return fmt.Errorf("invalid certificate secret: %s present in namespace: %s, please delete and try again.\nError: %s",
+				secretName, namespace, valErr)
+		}
+	}
+
+	return fmt.Errorf("valid certificate secret: %s already present in namespace: %s , skipping Certificate creation",
+		secretName, namespace)
+}
+
+func (cm *CertManagerProvider) ValidateCertificateSecret(
+	ctx context.Context,
+	secretName string,
+	namespace string,
+	_ *interfaces.Config) error {
+	secret := &corev1.Secret{}
+	err := cm.Get(ctx, client.ObjectKey{Name: secretName, Namespace: namespace}, secret)
+	if err != nil {
+		return err
+	}
+
+	certificateData, exists := secret.Data["tls.crt"]
+	if !exists {
+		return errors.New("certificate not found in secret")
+	}
+
+	decodeCertificatePem, _ := pem.Decode(certificateData)
+	if decodeCertificatePem == nil {
+		return errors.New("failed to decode PEM block")
+	}
+
+	privateKeyData, keyExists := secret.Data["tls.key"]
+	if !keyExists {
+		return errors.New("private key not found in secret")
+	}
+
+	parseCert, err := x509.ParseCertificate(decodeCertificatePem.Bytes)
+	if err != nil {
+		return fmt.Errorf("failed to parse certificate: %w", err)
+	}
+
+	if parseCert.NotAfter.Before(time.Now()) {
+		return errors.New("certificate has expired")
+	}
+
+	privateKey, err := parsePrivateKey(privateKeyData)
+	if err != nil {
+		return fmt.Errorf("failed to parse private key: %w", err)
+	}
+
+	if checkKeyPairErr := checkKeyPair(parseCert, privateKey); checkKeyPairErr != nil {
+		return fmt.Errorf("private key does not match certificate: %w", checkKeyPairErr)
+	}
+
+	return nil
+}
+
+func (cm *CertManagerProvider) DeleteCertificateSecret(ctx context.Context, secretName string, namespace string) error {
+	secret := &corev1.Secret{}
+	err := cm.Get(ctx, client.ObjectKey{Name: secretName, Namespace: namespace}, secret)
+	if err != nil {
+		return fmt.Errorf("failed to get secret: %w", err)
+	}
+
+	// Delete the Secret
+	err = cm.Delete(ctx, secret)
+	if err != nil {
+		return fmt.Errorf("failed to delete secret: %w", err)
+	}
+
+	return nil
+}
+
+func (cm *CertManagerProvider) RevokeCertificate(ctx context.Context, secretName string, namespace string) error {
+	cmCertificate := &certmanagerv1.Certificate{}
+	getCertificateErr := cm.Get(ctx, client.ObjectKey{Name: secretName, Namespace: namespace}, cmCertificate)
+	if getCertificateErr != nil {
+		return getCertificateErr
+	}
+
+	dErr := cm.Delete(ctx, cmCertificate)
+	if dErr != nil {
+		return dErr
+	}
+
+	// By default, cert-manager Certificate deletion does not delete the associated secret.
+	// Existing secret will allow to services relying on that Certificate, so additionally delete it
+	// More info: https://cert-manager.io/docs/usage/certificate/#cleaning-up-secrets-when-certificates-are-deleted
+	dSecretErr := cm.DeleteCertificateSecret(ctx, secretName, namespace)
+	if dSecretErr != nil {
+		if k8serrors.IsNotFound(dSecretErr) {
+			fmt.Println("Certificate secret not found, maybe already deleted")
+		} else {
+			return dSecretErr
+		}
+
+	}
+
+	return nil
+}
+
+func (cm *CertManagerProvider) GetCertificateConfig(
+	ctx context.Context,
+	secretName string,
+	namespace string) (*interfaces.Config, error) {
+	cmCertificate := &certmanagerv1.Certificate{}
+	err := cm.Get(ctx, client.ObjectKey{Name: secretName, Namespace: namespace}, cmCertificate)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get certificate: %w", err)
+	}
+
+	cfg := &interfaces.Config{
+		CommonName:   cmCertificate.Spec.CommonName,
+		Organization: cmCertificate.Spec.Subject.Organizations,
+		AltNames: interfaces.AltNames{
+			DNSNames: cmCertificate.Spec.DNSNames,
+			IPs:      make([]net.IP, len(cmCertificate.Spec.IPAddresses)),
+		},
+		ValidityDuration: cmCertificate.Spec.Duration.Duration,
+		ExtraConfig: map[string]any{
+			IssuerNameKey: cmCertificate.Spec.IssuerRef.Name,
+			IssuerKindKey: cmCertificate.Spec.IssuerRef.Kind,
+		},
+	}
+
+	return cfg, nil
+}
+
+// getExtraConfigByName checks if the ExtraConfig keys has been defined with a valid string
+func getExtraConfigByName(key string, cfg *interfaces.Config) (string, error) {
+	value, isValid := cfg.ExtraConfig[key].(string)
+	if !isValid {
+		return "", fmt.Errorf("value for %s not correctly provided, try again", key)
+	}
+	return value, nil
+}
+
+// checkIssuerExists checks for if the provided issuer is present in the cluster
+func (cm *CertManagerProvider) checkIssuerExists(issuerName, issuerKind, namespace string, ctx context.Context) error {
+	switch issuerKind {
+	case "Issuer":
+		issuer := &certmanagerv1.Issuer{}
+		err := cm.Get(ctx, client.ObjectKey{Name: issuerName, Namespace: namespace}, issuer)
+		if k8serrors.IsNotFound(err) {
+			return fmt.Errorf("issuer %s not found in namespace %s", issuerName, namespace)
+		}
+	case "ClusterIssuer":
+		clusterIssuer := &certmanagerv1.ClusterIssuer{}
+		err := cm.Get(ctx, client.ObjectKey{Name: issuerName}, clusterIssuer)
+		if k8serrors.IsNotFound(err) {
+			return fmt.Errorf("clusterIssuer %s not found", issuerName)
+		}
+	default:
+		return fmt.Errorf("unsupported issuer kind: %s", issuerKind)
+	}
+	return nil
+}
+
+// createCertificate creates a cert-manager Certificate resource in the specified namespace.
+// DNSNames and IPAddresses if not user-defined, will be set to default value in runtime:
+// fmt.Sprintf("%s-%d.%s.%s.svc.cluster.local", ec.Name, index, ec.Name, ec.Namespace)
+// returns an error if the Certificate resource cannot be created.
+func (cm *CertManagerProvider) createCertificate(
+	ctx context.Context,
+	secretName string,
+	namespace string,
+	cfg *interfaces.Config) error {
+	issuerName, err := getExtraConfigByName(IssuerNameKey, cfg)
+	if err != nil {
+		return err
+	}
+	issuerKind, err := getExtraConfigByName(IssuerKindKey, cfg)
+	if err != nil {
+		return err
+	}
+
+	checkIssuerExist := cm.checkIssuerExists(issuerName, issuerKind, namespace, ctx)
+	if checkIssuerExist != nil {
+		return checkIssuerExist
+	}
+
+	certificateResource := &certmanagerv1.Certificate{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      secretName,
+			Namespace: namespace,
+		},
+		Spec: certmanagerv1.CertificateSpec{
+			CommonName: cfg.CommonName,
+			Subject: &certmanagerv1.X509Subject{
+				Organizations: cfg.Organization,
+			},
+			SecretName:  secretName,
+			DNSNames:    cfg.AltNames.DNSNames,
+			IPAddresses: strings.Fields(strings.Trim(fmt.Sprint(cfg.AltNames.IPs), "[]")),
+			IssuerRef: cmmeta.ObjectReference{
+				Name: issuerName,
+				Kind: issuerKind,
+			},
+			Duration: &metav1.Duration{Duration: cfg.ValidityDuration},
+		},
+	}
+
+	return cm.Create(ctx, certificateResource)
+}
+
+// parsePrivateKey parses the private key from the PEM-encoded data.
+func parsePrivateKey(privateKeyData []byte) (crypto.PrivateKey, error) {
+	block, _ := pem.Decode(privateKeyData)
+	if block == nil {
+		return nil, errors.New("failed to decode private key: invalid PEM")
+	}
+
+	// Parse the private key from the PEM block
+	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		// Parse the private key in another format (e.g., RSA)
+		privateKey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse private key: %w", err)
+		}
+	}
+
+	return privateKey, nil
+}
+
+// checkKeyPair checks if the private key matches the certificate by validating the public key
+func checkKeyPair(cert *x509.Certificate, privateKey crypto.PrivateKey) error {
+	switch key := privateKey.(type) {
+	case *rsa.PrivateKey:
+		pub, ok := cert.PublicKey.(*rsa.PublicKey)
+		if !ok || !key.PublicKey.Equal(pub) {
+			return errors.New("private key(RSA) does not match the public key in the certificate")
+		}
+	case *ecdsa.PrivateKey:
+		pub, ok := cert.PublicKey.(*ecdsa.PublicKey)
+		if !ok || !key.PublicKey.Equal(pub) {
+			return errors.New("private key(ECDSA) does not match the public key in the certificate")
+		}
+	case *ed25519.PrivateKey:
+		pub, ok := cert.PublicKey.(ed25519.PublicKey)
+		if !ok || !bytes.Equal(key.Public().(ed25519.PublicKey), pub) {
+			return errors.New("private key(ED25519) does not match the public key in the certificate")
+		}
+	default:
+		return fmt.Errorf("unsupported private key type: %T", key)
+	}
+
+	return nil
+}
